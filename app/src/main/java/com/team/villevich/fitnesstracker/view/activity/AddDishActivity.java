bbff7afadf5422247479c300a0b33aba4a35c7f5
Dima Koval <dickjustice228@gmail.com>
2018-02-10 23:59:12 +0200

    Project created. Basic activities and fragments created

---
+++ app/src/main/java/com/team/villevich/fitnesstracker/view/activity/AddDishActivity.java
@@ -0,0 +1,15 @@
+package com.team.villevich.fitnesstracker.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.team.villevich.fitnesstracker.R;
+
+public class AddDishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dish);
+    }
+}
