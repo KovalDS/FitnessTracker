04e252df96abf49035bc9d0882dd7e7232c83730
wladeq <vlad-i@ukr.net>
2018-02-11 14:27:39 +0100

    Added radio button and sample UI test

---
+++ app/src/androidTest/java/com/team/villevich/fitnesstracker/view/activity/RegistrationTest.java
@@ -0,0 +1,152 @@
+package com.team.villevich.fitnesstracker.view.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.team.villevich.fitnesstracker.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RegistrationTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void test1() {
+        ViewInteraction appCompatAutoCompleteTextView = onView(
+                allOf(withId(R.id.email),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView.perform(scrollTo(), replaceText("vlad-i@ukr.net"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatEditText.perform(scrollTo(), replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.register_button), withText("Register"),
+                        childAtPosition(
+                                allOf(withId(R.id.email_login_form),
+                                        childAtPosition(
+                                                withId(R.id.login_form),
+                                                0)),
+                                3)));
+        appCompatButton.perform(scrollTo(), click());
+
+        ViewInteraction appCompatAutoCompleteTextView2 = onView(
+                allOf(withId(R.id.reg_email),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView2.perform(scrollTo(), replaceText("vlad"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.reg_password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatEditText2.perform(scrollTo(), replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction appCompatAutoCompleteTextView3 = onView(
+                allOf(withId(R.id.reg_weight),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView3.perform(scrollTo(), replaceText("32"), closeSoftKeyboard());
+
+        ViewInteraction appCompatAutoCompleteTextView4 = onView(
+                allOf(withId(R.id.reg_height),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView4.perform(scrollTo(), replaceText("321"), closeSoftKeyboard());
+
+        ViewInteraction appCompatAutoCompleteTextView5 = onView(
+                allOf(withId(R.id.reg_age),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView5.perform(scrollTo(), replaceText("54"), closeSoftKeyboard());
+
+        ViewInteraction appCompatRadioButton = onView(
+                allOf(withId(R.id.radio_btn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.email_login_form),
+                                        5),
+                                0)));
+        appCompatRadioButton.perform(scrollTo(), click());
+
+        ViewInteraction appCompatAutoCompleteTextView6 = onView(
+                allOf(withId(R.id.reg_daily_calories),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView6.perform(scrollTo(), replaceText("3214"), closeSoftKeyboard());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
